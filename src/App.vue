70094c2082951b3eb48f23d4c9dc4810d072a039
heyupeng <yupeng.he828@aliyun.com>
2018-08-07 10:46:00 +0800

    头部组件完成

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,8 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">CRUD</a>
-        </div>
-      </div>
-    </nav>
+    <!-- 网页的头部 -->
+    <app-header></app-header>
+
     <div class="container-fluid">
       <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
@@ -251,12 +241,13 @@
 </template>
 
 <script>
+// 导入APPHeader.vue
+import AppHeader from './components/AppHeader.vue';
+
 export default {
-  name: "app",
-  data() {
-    return {
-      msg: "Welcome to Your Vue.js App"
-    };
+  // 局部组件
+  components: {
+    AppHeader
   }
 };
 </script>
